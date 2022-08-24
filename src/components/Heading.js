007c1aa63e1e4bbaf6f22950d32e058f3a577e43
sianturiwilly <prajudiwilliam10@gmail.com>
2022-08-24 21:16:30 +0700

    Commit kedua

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Heading(){
+    return (
+        <div>
+            <h1>Heading 1</h1>
+            <p>Paragraf 1</p>
+            <hr />
+        </div>
+    )
+}
+
+export default Heading;
